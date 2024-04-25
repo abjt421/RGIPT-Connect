38754190f5f0507fc3825258059ad83d5a827f66
ABHIJEET PARASHAR <128897541+abjt421@users.noreply.github.com>
2024-04-26 02:03:09 +0530

    Add files via upload

---
+++ RGIPT Connect Main/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCYmbr-SSaAE48573nFpZQgs_FBzfVXwPU",
+    authDomain: "rgipt-connect-new.firebaseapp.com",
+    projectId: "rgipt-connect-new",
+    storageBucket: "rgipt-connect-new.appspot.com",
+    messagingSenderId: "477078870027",
+    appId: "1:477078870027:web:c1dfd5df99c1341dac4f0e",
+    measurementId: "G-4F530DSWFD"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
